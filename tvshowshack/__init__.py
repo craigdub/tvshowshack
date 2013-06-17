1a1ffb62fa4427d7e1b149e3439d478e6bff8384
craigdub <craigdub746@gmail.com>
2013-06-16 18:46:58 -0700

    initial comit of proj start

--- tvshowshack/__init__.py
+++ tvshowshack/__init__.py
@@ -1,23 +1,116 @@
 from pyramid.config import Configurator
 from pyramid_jinja2 import renderer_factory
 from tvshowshack.models import get_root
+import deform
+from jinja2 import Environment
+from jinja2 import FileSystemLoader
+from pkg_resources import resource_filename
+from pyramid.threadlocal import get_current_request
+from translationstring import TranslationStringFactory
+from pyramid.i18n import get_localizer
+from urlparse import urlparse
+import pymongo
+
+class PyramidTranslator(object):
+
+    def __init__(self, domain='deform'):
+        self.domain = domain
+
+    def translate(self, term):
+
+        if not hasattr(term, 'interpolate'): # not a translation string
+            term = TranslationStringFactory(self.domain)(term)
+        return get_localizer(get_current_request()).translate(term)
+
+    def pluralize(self, singular, plural, n):
+        localizer = get_localizer(get_current_request())
+        return localizer.pluralize(singular, plural, n)
+
+
+    def gettext(self, message):
+        return self.translate(message)
+
+    def ngettext(self, singular, plural, n):
+        return self.pluralize(singular, plural, n)
+
+class jinja2_renderer_factory(object):
+    def __init__(self, search_paths=(), default_templates='tvshowshack:bootstrap_templates',
+            translator=None, extensions=[]):
+
+        if 'jinja2.ext.i18n' not in extensions:
+           extensions.append('jinja2.ext.i18n')
+
+        self.env = Environment(extensions=extensions)
+        self.env.loader = FileSystemLoader(())
+
+        for path in search_paths:
+            self.add_search_path(path)
+
+        if translator == None:
+            translator = DummyTranslator
+
+        self.env.install_gettext_callables(translator.gettext, translator.ngettext)
+
+        self.add_search_path(default_templates)
+
+    def add_search_path(self, path):
+        self.env.loader.searchpath.append(resource_filename(*(path.split(':'))))
+
+    def add_filter(self, name, func):
+        self.env.filters[name] = func
+
+    def __call__(self, tname, **kw):
+        if not '.jinja2' in tname:
+            tname += '.jinja2'
+
+        template = self.env.get_template(tname)
+        return template.render(**kw)
+
+
+def includeme(config):
+    settings = config.registry.settings
+    domain = settings.get('deform_jinja2.i18n.domain', 'deform')
+    search_path = settings.get('deform_jinja2.template_search_path', '').strip()
+    renderer = jinja2_renderer_factory(search_paths=search_path.split(), 
+            translator=PyramidTranslator(domain=domain))
+    deform.Form.set_default_renderer(renderer)
+
+
+    def add_filter(config, name, func):
+        renderer.add_filter(name, func)
+
+    config.add_directive('add_jinja2_filter', add_filter)
+
+    config.registry['deform_jinja2_renderer'] = renderer
 
 def main(global_config, **settings):
-    """ This function returns a WSGI application.
-    
-    It is usually called by the PasteDeploy framework during 
-    ``paster serve``.
-    """
-    settings = dict(settings)
-    settings.setdefault('jinja2.i18n.domain', 'tvshowshack')
+		""" This function returns a WSGI application.
+		It is usually called by the PasteDeploy framework during 
+		``paster serve``.
+		"""
+		settings = dict(settings)
+		settings.setdefault('jinja2.i18n.domain', 'tvshowshack')
+		config = Configurator(root_factory=get_root, settings=settings)
+		config.add_static_view('static', 'static', cache_max_age=3600)
+		config.add_translation_dirs('locale/')
+		config.scan('tvshowshack')
+		config.add_route('home', '/')
+		config.add_route('signup', '/signup')
+		db_url = urlparse(settings['mongo_uri'])
+		config.registry.db = pymongo.Connection(
+											host=db_url.hostname,
+											port=db_url.port,
+											)
+		includeme(config)
+		
+		def add_db(request):
+			db = config.registry.db[db_url.path[1:]]
+			if db_url.username and db_url.password:
+				db.authenticate(db_url.username, db_url.password)
+			return db
+           
+		config.add_request_method(add_db, 'db', reify=True)
 
-    config = Configurator(root_factory=get_root, settings=settings)
-    config.add_translation_dirs('locale/')
-    config.include('pyramid_jinja2')
+		return config.make_wsgi_app()
+	
 
-    config.add_static_view('static', 'static')
-    config.add_view('tvshowshack.views.my_view',
-                    context='tvshowshack.models.MyModel', 
-                    renderer="mytemplate.jinja2")
-
-    return config.make_wsgi_app()
--- tvshowshack/views.py
+++ tvshowshack/views.py
@@ -1,6 +1,84 @@
 from pyramid.i18n import TranslationStringFactory
+import colander
+from deform import Form
+from pyramid.view import view_config
+from colander import Schema, SchemaNode, String
+from deform.widget import RichTextWidget, TextAreaWidget
+from pyramid_deform import FormView
+import deform
+from pyramid.renderers import render_to_response
+import os
 
 _ = TranslationStringFactory('tvshowshack')
+"""
+	class PageSchema(Schema):
+		title = SchemaNode(String())
+		description = SchemaNode(
+		String(),
+		widget=TextAreaWidget(cols=40, rows=5),
+		missing=u"",
+		)
+		body = SchemaNode(
+			String(),
+			widget=RichTextWidget(),
+			missing=u"",
+		)
+"""
 
+"""
+	@view_config(route_name='home', renderer='form.pt')
+	class PageEditView(FormView):
+		schema = PageSchema()
+		buttons = ('save',)
+		form_options = (('formid', 'pyramid-deform'),('method', 'GET'))
+		@view_config(request_param='form.save')
+		def save_success(self, appstruct):
+			context = self.request.context
+			context.title = appstruct['title']
+			context.description = appstruct['description']
+			context.body = appstruct['body']
+			self.request.session.flash(u"Your changes have been saved.")
+			return HTTPFound(location=self.request.path_url)
+		def appstruct(self):
+			return {'title': context.title,'description': context.description,'body': context.body}
+"""
+class Person(colander.MappingSchema):
+	email = colander.SchemaNode(colander.String())
+	password1 = colander.SchemaNode(
+								colander.String(),
+								validator=colander.Length(min=5, max=100),
+								widget=deform.widget.PasswordWidget(size=20),
+								description='Enter a password')
+	password2 = colander.SchemaNode(
+								colander.String(),
+								validator=colander.Length(min=5, max=100),
+								widget=deform.widget.PasswordWidget(size=20),
+								description='Enter a password')
+
+class ProjectorViews(object):
+	def __init__(self, request):
+		self.request = request
+	def site_view(self):
+		schema = Person()
+		myform = deform.Form(schema, buttons=('submit',))
+		print os.getcwd()
+		return myform
+
+def headerInc(dir, p=os.getcwd()+'/tvshowshack/static'):
+	pth = os.path.join(p,dir)
+	return os.listdir(pth)
+
+@view_config(route_name='signup', renderer='templates/mytemplate.jinja2')
 def my_view(request):
-    return {'project':'tvshowshack'}
+	inst = ProjectorViews(request)
+	frmSignup = inst.site_view()
+	if 'email' in request.POST:
+		controls = request.POST.items()
+		import pdb;pdb.set_trace()
+		request.db['users'].insert({"email":db.request.POST['email'], "password":db.request.POST['password1']}) 
+		try:
+			appstruct = frmSignup.validate(controls)
+		except deform.ValidationFailure, e:
+			return {'form':e, "cssInc":headerInc('css'), "jsInc":headerInc('js')}
+	return {"form":frmSignup,"cssInc":headerInc('css'), "jsInc":headerInc('js')}
+
